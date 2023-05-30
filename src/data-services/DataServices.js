61822f2b7286066db3cfc25ead13bffcb8cccc47
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-05-30 17:13:25 -0600

    Test changes

--- src/data-services/DataServices.js
+++ src/data-services/DataServices.js
@@ -1,52 +1,53 @@
 //Functions for API integration and to fetch the API data
-// const CORS_URL = 'https://cors-anywhere.herokuapp.com/' 
-const API_URL = `https://torre.bio/api`
+// const CORS_URL = 'https://cors-anywhere.herokuapp.com/'
+const API_URL = `https://torre.bio/api`;
 
 const defaultUser = {
-  person : {
+  person: {
     name: "",
-    picture:""
+    picture: "",
   },
-  strengths : []
-}
+  strengths: [],
+};
 
-
-function groupByProficiency(data){
-  return data.reduce((result,item) => {
-    const {proficiency} = item;
-    if(!result[proficiency]){
+function groupByProficiency(data) {
+  return data.reduce((result, item) => {
+    const { proficiency } = item;
+    if (!result[proficiency]) {
       result[proficiency] = [];
     }
     result[proficiency].push(item);
     return result;
-  },{});
+  }, {});
 }
 
 export async function fetchUserData(username) {
-    try {
-      const response = await fetch(`${API_URL}/bios/${username}`, {method:'GET',mode:'no-cors'});
+  try {
+    const options = {
+      method: "GET",
+      mode: "no-cors",
+    };
+    const response = await fetch(`${API_URL}/bios/${username}`, options);
 
-      const data = await response.json();
-      console.log(data)
-      const {person,strengths} = data || defaultUser;
+    const data = await response.json();
+    console.log(await data);
+    const { person, strengths } = data || defaultUser;
 
-      const skills = groupByProficiency(strengths)||[];
-      return {person,skills};
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-      return defaultUser;
-    }
+    const skills = groupByProficiency(strengths) || [];
+    return { person, skills };
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return defaultUser;
   }
-  
-  export async function fetchSkillsData(id) {
-    try {
-      const response = await fetch(`${API_URL}/opportunities/${id}`);
-      const data = await response.json();
-      return data || []; 
-    } catch (error) {
-      console.error('Error fetching skills data:', error);
-      return [];
-    }
+}
+
+export async function fetchSkillsData(id) {
+  try {
+    const response = await fetch(`${API_URL}/opportunities/${id}`);
+    const data = await response.json();
+    return data || [];
+  } catch (error) {
+    console.error("Error fetching skills data:", error);
+    return [];
   }
-  
-  
+}
