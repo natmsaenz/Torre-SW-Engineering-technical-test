79ab97bf58a7a607e6a21f4c7e30f3c968ac774c
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-05-30 14:04:41 -0600

    changed the routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,22 +1,33 @@
 import React from "react";// eslint-disable-line no-unused-vars
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter, // eslint-disable-line no-unused-vars
+  RouterProvider, Router, Switch, Route // eslint-disable-line no-unused-vars
 } from "react-router-dom";
 import Profile from "../components/profile/Profile.jsx";
 import "../index.css";
 
-let router = createBrowserRouter([
-  {
-    path: "/profile/:username",
-    element: <Profile />,
-  },
-]);
+// let router = createBrowserRouter([
+//   {
+//     path: "/profile/:username",
+//     element: <Profile />,
+//   },
+// ]);
 
 export default function App() {
-  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+  return (
+    <div className="App">
+     <Router>
+       <Switch>
+         <Route path='/Torre-SW-Engineering-technical-test' exact component={Profile}/>
+         <Route path='/Torre-SW-Engineering-technical-test/profile/natmsaenz25' exact component={Profile}/>
+       </Switch>
+     </Router>
+    </div>
+  );
+
+  // return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
 }
 
-if (import.meta.hot) {
-  import.meta.hot.dispose(() => router.dispose());
-}
+// if (import.meta.hot) {
+//   import.meta.hot.dispose(() => router.dispose());
+// }
