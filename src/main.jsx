42879280b60ab93efcf10f564b5eb31ecd2b90d1
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-05-30 12:56:26 -0600

    added proper routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./router/Router.jsx";
+import { Link, HashRouter } from 'react-router-dom' // eslint-disable-line no-unused-vars
+
 import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  
-    <App />
+  <HashRouter>
+  <App />
+
+  </HashRouter>
   </React.StrictMode>
 );
 
