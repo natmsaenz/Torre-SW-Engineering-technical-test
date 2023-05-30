7a64aa15849b62826d2eb389b202ade99b8591f2
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-05-30 15:22:14 -0600

    Revert changes and merges

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,32 +1,22 @@
 import React from "react";// eslint-disable-line no-unused-vars
 import {
-  BrowserRouter, // eslint-disable-line no-unused-vars
-  RouterProvider, Router, Switch, Route // eslint-disable-line no-unused-vars
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
 import Profile from "../components/profile/Profile.jsx";
 import "../index.css";
 
-// let router = createBrowserRouter([
-//   {
-//     path: "/profile/:username",
-//     element: <Profile />,
-//   },
-// ]);
+let router = createBrowserRouter([
+  {
+    path: "/profile/:username",
+    element: <Profile />,
+  },
+]);
 
 export default function App() {
-  return (
-    <div className="App">
-     <Router>
-       <Switch>
-         <Route path='/Torre-SW-Engineering-technical-test' exact component={Profile}/>
-       </Switch>
-     </Router>
-    </div>
-  );
-
-  // return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
 }
 
-// if (import.meta.hot) {
-//   import.meta.hot.dispose(() => router.dispose());
-// }
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => router.dispose());
+}
