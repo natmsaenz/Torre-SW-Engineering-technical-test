c18f2a93b6e5ab5ff046d0a7fbd4f9dbd609081b
Natalia Madrigal Saenz <natalia.madrigal2941@alumnos.udg.mx>
2023-05-30 22:45:27 -0600

    Test changes

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Torre-SW-Engineering-technical-test",
-  server:{
-    proxy: {
-      '/api': 'https://bio.torre.co/api'
-    }
-  },
   plugins: [react()],
 })
